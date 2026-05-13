9f7131bd27c1c1fe2d8cad9ad75ab4dc140ee45e
Adrian Chiriac <adrian_gecala@yahoo.com>
2026-05-13 12:52:02 +0300

    add scalar specification + CORS policy in program.cs

--- backend/Ecommerce.Api/Program.cs
+++ backend/Ecommerce.Api/Program.cs
@@ -1,17 +1,32 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") //will replace later w/ angular FE
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors("AllowFrontend"); 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
